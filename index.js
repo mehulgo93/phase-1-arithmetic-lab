42d50e8f217b3ff6fa43d121ce73485812eb3bdd
Mehul <mehul_angle@hotmail.com>
2023-06-13 16:18:19 -0400

    Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+const multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(5,6,7,20);
+
+
